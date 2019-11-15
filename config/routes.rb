9af259b2a516f837cd9e6d133cc50fb38497a967
Fukuoka-Ryo <frbaseball9452@docomo.ne.jp>
2019-11-16 08:57:50 +0900

    modify file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  #/users => users#index
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+
   root 'application#hello'
 
 end
