2de607a7a65609443dbd272ebc17820a5bd0475a
Fukuoka-Ryo <frbaseball9452@docomo.ne.jp>
2019-11-17 09:48:38 +0900

    Finish section5

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def home
+    # app/views/
+    #app/views/static_pages/home.
+  end
+
+  def help
+  end
+  def about
+  end
+  def contact
+  end
+
+  end
+end
